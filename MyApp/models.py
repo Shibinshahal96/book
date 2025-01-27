585c1e0cf6c01c041c026baaefb2cbb4379efb76
Shibinshaha-l <shibinshahal.pk@gmail.com>
2025-01-27 17:31:08 +0530

    first commit

--- MyApp/models.py
+++ MyApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    publisher = models.CharField(max_length=100)
+    pubdate = models.DateField()
+    
+    def __str__(self):
+        return self.title
+    
--- MyApp/views.py
+++ MyApp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 
 # Create your views here.
+from .models import book
+
+def booklist(request):
+    books = book.objects.all()
+    
+    return render(request, 'MyApp/booklist.html', {'books': books})
+
+def bookcreate(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        price = request.POST.get('price')
+        publisher = request.POST.get('publisher')
+        pubdate = request.POST.get('pubdate')
+        
+        book.objects.create(title=title, author=author, price=price, publisher=publisher, pubdate=pubdate)
+        return redirect("booklist")
+    return render(request, 'MyApp/books_forms.html')
+
